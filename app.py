d9e46b537a89a86122a261584fd38600dd135e3f
Gaurav2ds <gauravchordiya.ds@gmail.com>
2022-09-27 17:30:01 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,44 @@
+from smtplib import SMTPRecipientsRefused
+from flask import Flask ,request,render_template
+import pickle
+import numpy as np
+app = Flask(__name__)
+model1 = pickle.load(open('model.pkl','rb'))
+
+@app.route("/")
+def index():
+	
+	return render_template("index.html")
+
+@app.route("/irish", methods=['POST', 'GET'])
+def irish():
+    input_data=np.zeros(3)
+
+
+    SepalLengthCm=request.form['SepalLengthCm']
+    SepalWidthCm=request.form['SepalWidthCm']
+    PetalLengthCm=request.form['PetalLengthCm']
+    PetalWidthCm=request.form['PetalWidthCm']
+    print(SepalLengthCm)
+    prediction= model1.predict([[SepalLengthCm,SepalWidthCm,PetalLengthCm,PetalWidthCm]])
+    
+
+    Species=prediction[0]
+    if Species == 0:
+        Specie="Setosa"
+
+    elif Species ==1:
+        Specie="Versicolor"
+    elif Species ==2:
+        Specie ="Virginica"
+        
+
+
+
+
+
+    return render_template("index.html",Species=Specie)
+	
+
+if __name__ == "__main__":
+    app.run(debug=True)
